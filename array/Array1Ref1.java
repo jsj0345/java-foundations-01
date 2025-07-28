6b88e244d116280deafecdb760be75f9964a8878
jsj <a01099350974@gmail.com>
2025-07-28 23:25:50 +0900

    chore: 커밋 테스트

--- array/Array1Ref1.java
+++ array/Array1Ref1.java
@@ -19,5 +19,6 @@ public class Array1Ref1 {
         System.out.println("학생3 점수: " + students[2]);
         System.out.println("학생4 점수: " + students[3]);
         System.out.println("학생5 점수: " + students[4]);
+        System.out.println("학생6 점수:");
     }
 }
